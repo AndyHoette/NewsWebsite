a76a063241d05e7817bd8ca9c248d25470f4f760
Joshua Lee <j.m.lee@wustl.edu>
2024-09-27 15:35:31 -0500

    1

--- storypage.php
+++ storypage.php
@@ -7,12 +7,23 @@
 </head>
 
 
-
 <body>
     <?php
     require "database.php";
-
     $sqlStory = "SELECT title, body, link FROM Stories";
+
+    $result = $conn->query($sqlStory);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<br> id: " . $row["title"] . " - Name: " . $row["body"] . " " . $row["link"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
 
     ?>
 </body>
