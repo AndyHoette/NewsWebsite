162beda902d459a1e997f3aebe7545b11d4a2b8b
Andy-east <a.h.hoette@wustl.edu>
2024-09-27 14:24:49 -0500

    Added HTML for Login

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Log In</title>
+</head>
+<body>
+<h1>Existing User</h1>
+<form name="LogIn" action="processLogin.php" method="get" autocomplete="off">
+    <p>
+        <label for="user">Username:</label>
+        <input type="text" name="user" id="user" /> <!--creates a simple form with entry and a submit-->
+        <label for="password">Password:</label>
+        <input type="text" name="password" id="password" /> <!--creates a simple form with entry and a submit-->
+    </p>
+    <p>
+        <input type="submit" value="Log In!" />
+    </p>
+</form>
+<h1>New User</h1>
+<form name="createUser" action="processAccountCreation.php" method="get" autocomplete="off">
+    <p>
+        <label for="user">Username:</label>
+        <input type="text" name="user" id="user" /> <!--creates a simple form with entry and a submit-->
+        <label for="password">Password:</label>
+        <input type="text" name="password" id="password" /> <!--creates a simple form with entry and a submit-->
+    </p>
+    <p>
+        <input type="submit" value="Create Account!" />
+    </p>
+</form>
+</body>
+</html>
