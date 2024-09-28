939c404ef7f655b8b8a6af847e16d3c7ccec7c9b
Andy-east <a.h.hoette@wustl.edu>
2024-09-28 16:08:07 -0500

    removed session start from pAC

--- processAccountCreation.php
+++ processAccountCreation.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <?php
-session_start();
+//session_start();
 if(!(isset($_POST['token'])&&hash_equals($_POST['token'],$_SESSION['token']))){
     header("Location: unauthorized.php");
 }
