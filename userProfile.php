54c4fce732a210f9025c1e68866ba3baa6ad9d4e
Andy-east <a.h.hoette@wustl.edu>
2024-09-29 13:21:32 -0500

    seeing what bio prints when null

--- userProfile.php
+++ userProfile.php
@@ -8,4 +8,15 @@ else{
     echo "<p>uh oh</p>";
     //header("Location: home.php");
 }
+$mysqli = new mysqli('localhost', 'viewer', 'easyPassword', 'newsWebsite');
+
+if ($mysqli->connect_errno) {
+    printf("Connection Failed: %s\n", $mysqli->connect_error);
+    exit;
+}
+$stmt = $mysqli->prepare("Select userName, bio from Users where userName = ?");
+$stmt->bind_param("s", $_SESSION['userName']);
+$stmt->execute();
+$stmt->bind_result($userName, $bio);
+echo "<h1>" . $userName . "</h1>";
 ?>
