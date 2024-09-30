35362d9f09558c630afbcb6c89dfdc078cbb1a02
Andy-east <a.h.hoette@wustl.edu>
2024-09-29 22:24:42 -0500

    everything should work except the edit/delete button

--- deleteStory.php
+++ deleteStory.php
@@ -3,3 +3,14 @@
 //then check if userName and userCreated are the same
 //then delete all the comments associated with the story
 //then delete the story
+session_start();
+if(!isset($_POST['token'])||!hash_equals($_POST['token'],$_SESSION['token'])){ //makes sure this isn't a CSRF
+    header("Location: unauthorized.php");
+}
+require "database.php";
+$stmt = $mysqli->prepare("delete from Stories where id=?");
+$stmt->bind_param("i",$_POST['storyID']);
+$stmt->execute();
+$stmt->close();
+header("Location: userProfile.php");
+exit;
--- processStoryEdit.php
+++ processStoryEdit.php
@@ -1,4 +1,15 @@
 <?php
-//check CSRF tokens
-//check userName is correct
-//just update the fields
+session_start();
+if(!isset($_POST['token'])||!hash_equals($_POST['token'],$_SESSION['token'])){ //makes sure this isn't a CSRF
+    header("Location: unauthorized.php");
+}
+require "database.php";
+$newTitle = $_POST['title'];
+$newStory = $_POST['story'];
+$newLink = $_POST['link'];
+$stmt = $mysqli->prepare("update Stories set title=?, story=?, link=? where id=?");
+$stmt->bind_param("sssi", $newTitle, $newStory, $newLink, $_POST['storyID']);
+$stmt->execute();
+$stmt->close();
+header("Location: userProfile.php");
+exit;
